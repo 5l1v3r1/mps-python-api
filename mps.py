eaa26b42fedd4ebb86b03fbc3b341c3447bb8ece
Furkan Kalkan <furkan.kalkan3@gmail.com>
2018-02-11 03:46:14 +0200

    ilk sürüm
    Paket veritabanını güncelleme, kurulu kontrol, bağımlılık listeleme ve paket dosyası indirme özellikleri tamamlandı.

---
+++ mps.py
@@ -0,0 +1,133 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  mps.py
+#  
+#  Copyright 2018 geekdinazor <furkan.kalkan3@gmail.com>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+#  
+
+import os,urllib.error,shutil
+import urllib.request
+class API :
+	""" Milis Paket Sistemi işlemleri için native Python kütüphaneleri """
+	
+	def __init__ (self):
+		self.paketDizin="/var/lib/pkg/DB"
+		self.ayarDosya="/etc/mps.conf"
+		self.ayarlar={}
+		self.__ayarDosyaOku()
+		#self.paketDepo="/depo/paketler/"
+		self.paketDepo=""
+		self.talimatDepo="/root/talimatname"
+	
+	def __ayarDosyaOku(self):
+		f=open(self.ayarDosya,'r')
+		satirlar=f.readlines()
+		if satirlar[0][0]!='#':
+			self.ayarlar["sunucu"] = satirlar[0].split('"')[1::2][0]
+		if satirlar[1][0]!='#':
+			self.ayarlar["sunucular"] = satirlar[1].split('"')[1::2][0].split(' ')
+		if satirlar[2][0]!='#':
+			self.ayarlar["lokal"] = satirlar[1].split('"')[1::2][0]
+		if satirlar[3][0]!='#':
+			self.ayarlar["docsil"] = satirlar[1].split('"')[1::2][0]
+		if satirlar[4][0]!='#':
+			self.ayarlar["yerelsil"] = satirlar[1].split('"')[1::2][0]
+		if satirlar[5][0]!='#':
+			self.ayarlar["ektalimatname"] = satirlar[1].split('"')[1::2][0]
+
+	def dosyaIndir(self,url,dosya,sessiz):
+		f=open(dosya, 'wb')
+		parca_boyut=16*1024
+		if sessiz:
+			try:
+				u=urllib.request.urlopen(url)
+				while True:
+					parca = u.read(parca_boyut)
+					if not parca:
+						break
+					f.write(parca)
+				yield None
+			except urllib.error.HTTPError as e:
+				yield e
+				os.remove(dosya)
+		else:
+			try:
+				u=urllib.request.urlopen(url)
+				i=0
+				toplam_boyut=u.length
+				while True:
+					i+=1
+					parca = u.read(parca_boyut)
+					if not parca:
+						break
+					yield ((i*parca_boyut)/toplam_boyut)*100
+					f.write(parca)
+			except urllib.error.HTTPError as e:
+				print(e)
+				os.remove(dosya)
+	
+	def paketVTGuncelle(self):
+		i=0
+		for sunucu in self.ayarlar["sunucular"]:
+			i+=1
+			if i > 1:
+				paketvt="{}paket{}.vt".format(self.paketDepo,i)
+			else:
+				paketvt="{}paket.vt".format(self.paketDepo)	
+			url="http://{}{}".format(sunucu,paketvt)
+			for hatalar in self.dosyaIndir(url,paketvt,sessiz=True):
+				yield [sunucu,hatalar]
+
+	def paketIndir(self,paket):
+		i=0
+		dosya=None
+		for sunucu in self.ayarlar["sunucular"]:
+			i+=1
+			if i > 1:
+				paketvt="{}paket{}.vt".format(self.paketDepo,i)
+			else:
+				paketvt="{}paket.vt".format(self.paketDepo)	
+				
+				f=open(paketvt,'r')
+				f=f.read().replace("\n"," ").split(" ")
+				try:
+					dosya=f[f.index(paket)+2]	
+					url="http://{}{}".format(sunucu,dosya.replace("#","%23"))
+				except ValueError as e:
+					yield "İstenilen paket veritabanında bulunamadı."
+		if dosya:
+			for cikti in self.dosyaIndir(url,dosya,sessiz=False):
+				yield cikti
+	
+	def kuruluKontrol(self, paket):
+		if paket in os.listdir(self.paketDizin):
+			return True
+		else:
+			return False	
+
+	def bagimliPaketListele(self,paket):
+		for dizin, altdizin, dosya in os.walk(self.talimatDepo):
+			if dizin.split("/")[-1] == paket:
+				talimat=open("{}/talimat".format(dizin),"r")
+				bagimlilar=set(talimat.read().split('\n')[3].split(':')[1].split(' '))
+				if '' in bagimlilar:
+					bagimlilar.remove('')
+				return bagimlilar
+
---
+++ ornekler.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  run.py
+#  
+#  Copyright 2018 geekdinazor <furkan.kalkan3@gmail.com>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+#  
+from mps import API
+
+if __name__ == '__main__':
+	mps = API()
+	
+	paket="kernel"
+
+	
+	print("#Paket Veritabanı Güncelleme")
+
+	for hatalar in mps.paketVTGuncelle():
+		sunucu=hatalar[0]
+		hata=hatalar[1]
+		if hata:
+			print("{} sunucusuna bağlanırken hata oluştu: \n{}".format(sunucu,hata))
+		else:
+			print("Paket veritabanı {} sunucusundan başarıyla güncellendi.".format(sunucu))
+
+
+	print("# Kurulu kontrol")
+
+	if mps.kuruluKontrol(paket):
+		print("{} sisteminizde kurulu".format(paket))
+	else:
+		print("{} sisteminizde kurulu değil".format(paket))
+
+	
+	
+	print("#Bağımlılık Listeleme")
+	
+	for i in  mps.bagimliPaketListele(paket):
+		print(i)
+
+
+
+	print("#Paket Dosyası İndirme")
+	
+	for cikti in mps.paketIndir(paket):
+		if type(cikti) == float:
+			print("{} %{:3.2f} indiriliyor.".format(paket,cikti))
+		else:
+			print("{} indirilirken hata oluştu: \n{}".format(paket,cikti))
+
